587ef05c76d18f9c0701a58827e6ec033ad287d5
pixelwebdevelopers <pixelwebdevelopers1@gmail.com>
2026-04-14 19:58:29 +0500

    client changes

--- src/pages/Students/PrintAdmissionForm.jsx
+++ src/pages/Students/PrintAdmissionForm.jsx
@@ -1,6 +1,7 @@
 import React, { forwardRef } from 'react';
+import PrintFooter from '@/components/common/PrintFooter';
 
-const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
+const PrintAdmissionForm = forwardRef(({ student }, ref) => {
     if (!student) return null;
 
     const createdAt = new Date(student.createdAt);
@@ -17,7 +18,7 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
     ).toFixed(2);
 
     return (
-        <div ref={ref} className="print-admission-form print:p-0 p-8 bg-white text-black font-serif text-[12px] leading-relaxed">
+        <div ref={ref} className="print-admission-form print:p-0 p-8 bg-white text-black font-serif text-[12px] leading-relaxed relative">
             <style>
                 {`
                 @media print {
@@ -85,6 +86,15 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
                     justify-content: center;
                     margin-left: -1px;
                 }
+                .date-box-large {
+                    border: 1px solid black;
+                    width: 60px;
+                    height: 20px;
+                    display: inline-flex;
+                    align-items: center;
+                    justify-content: center;
+                    margin-left: -1px;
+                }
                 .no-margin-last {
                     margin-right: 0;
                 }
@@ -114,7 +124,7 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
                     </div>
                     <div className="flex flex-col items-center">
                         <span className="text-[8px] uppercase">Year</span>
-                        <div className="date-box w-[50px]">{year}</div>
+                        <div className="date-box-large">{year}</div>
                     </div>
                 </div>
                 <div>
@@ -126,14 +136,14 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
             </div>
 
             {/* Student Information Section */}
-            <div className="border border-black p-1 mb-2">
-                <div className="flex items-center mb-2">
+            <div className="border border-black p-2 mb-2">
+                <div className="flex items-center mb-2 mt-1">
                     <div className="section-header">Student Information</div>
-                    <span className="font-bold ml-1">Reference in School.</span>
-                    <span className="field-line flex-1 ml-1">{student.referenceInSchool}</span>
+                    <span className="font-bold ml-1 mt-3">Reference in School.</span>
+                    <span className="field-line flex-1 ml-1 mt-3">{student.referenceInSchool}</span>
                 </div>
 
-                <div className="grid grid-cols-2 gap-y-2 mb-2">
+                <div className="grid grid-cols-2 gap-y-2 mb-2 mt-1">
                     <div className="flex items-end">
                         <span className="font-bold mr-1">Student Name:</span>
                         <span className="field-line flex-1 capitalize">{student.name}</span>
@@ -144,7 +154,7 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-2 gap-y-2 mb-2">
+                <div className="grid grid-cols-2 gap-y-2 mb-2 mt-1">
                     <div className="flex items-end">
                         <span className="font-bold mr-1">Form B / NIC No.</span>
                         <span className="field-line flex-1">{student.formBNicNo}</span>
@@ -155,7 +165,7 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-4 gap-2 mb-2">
+                <div className="grid grid-cols-4 gap-2 mb-2 mt-1">
                     <div className="flex items-end col-span-1">
                         <span className="font-bold mr-1">Casts.</span>
                         <span className="field-line flex-1">{student.caste}</span>
@@ -174,23 +184,23 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-3 gap-2 mb-2">
-                    <div className="flex items-end">
+                <div className="flex gap-2 mb-2 mt-1">
+                    <div className="flex gap-2">
                         <span className="font-bold mr-1">Hifz-E-Quran.</span>
-                        <span className="field-line flex-1"></span>
+                        <span className="field-line flex-1 w-[50px]"></span>
                     </div>
-                    <div className="flex items-end">
+                    <div className="flex gap-2">
                         <span className="font-bold mr-1">Medical Problem.</span>
-                        <span className="field-line flex-1">{student.specialInfo}</span>
+                        <span className="field-line flex-2 w-[220px]">{student.specialInfo}</span>
                     </div>
-                    <div className="flex items-end justify-end">
+                    <div className="flex ">
                         <span className="font-bold mr-1">Vaccine:</span>
                         <span className="mr-1">Yes</span> <div className="checkbox-box mr-2"></div>
                         <span className="mr-1">No</span> <div className="checkbox-box"></div>
                     </div>
                 </div>
 
-                <div className="flex items-center">
+                <div className="flex items-center mt-1">
                     <span className="font-bold text-[10px]">Academy is Compulsory for Board Classes (9th, 10th, 11th & 12th) students:</span>
                     <div className="flex items-center ml-auto">
                         <span className="mr-1">Yes</span> <div className="checkbox-box mr-2"></div>
@@ -200,7 +210,7 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
             </div>
 
             {/* Father / Guardian Section */}
-            <div className="border border-black p-1 mb-2">
+            <div className="border border-black p-2 mb-2">
                 <div className="section-header mb-2">Father / Guardian</div>
 
                 <div className="grid grid-cols-2 gap-4 mb-2">
@@ -214,7 +224,7 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-2 gap-4 mb-1">
+                <div className="grid grid-cols-2 gap-4 mb-1 mt-1">
                     <div className="flex items-end">
                         <span className="font-bold mr-1">Guardian Name (if any)</span>
                         <span className="field-line flex-1">{student.guardianName}</span>
@@ -227,55 +237,59 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
             </div>
 
             {/* Residential Address Section */}
-            <div className="border border-black p-1 mb-2">
-                <div className="flex items-center mb-2">
+            <div className="border border-black p-2 mb-2">
+                <div className="flex items-center mb-2 mt-1">
                     <div className="section-header">Residential Address</div>
-                    <span className="font-bold mr-1">H. No.</span>
-                    <span className="field-line min-w-[100px] mr-4">{student.houseNo}</span>
-                    <span className="font-bold mr-1">Street No.</span>
-                    <span className="field-line min-w-[150px] mr-4">{student.streetNo}</span>
-                    <span className="font-bold mr-1">Block /Phase.</span>
-                    <span className="field-line flex-1">{student.blockPhase}</span>
+                    <span className="font-bold mr-1 mt-3">H. No.</span>
+                    <span className="field-line min-w-[100px] mr-4 mt-3">{student.houseNo}</span>
+                    <span className="font-bold mr-1 mt-3">Street No.</span>
+                    <span className="field-line min-w-[150px] mr-4 mt-3">{student.streetNo}</span>
+                    <span className="font-bold mr-1 mt-3">Block /Phase.</span>
+                    <span className="field-line flex-1 mt-3">{student.blockPhase}</span>
                 </div>
 
-                <div className="flex items-start mb-1">
+                <div className="flex items-start mb-1 mt-1">
                     <div className="flex-1">
                         <span className="font-bold mr-1">Mohallah / Colony:</span>
                         <span className="field-line w-full mb-1">{student.mohallahColony}</span>
                     </div>
-                    <div className="ml-4 border-l border-black pl-2 flex flex-col gap-2">
-                        <div className="flex gap-4">
-                            <span className="font-bold text-[10px]">Cell 1.</span>
-                            <span className="field-line min-w-[150px]">{student.cell1}</span>
-                            <span className="font-bold text-[10px]">2.</span>
-                            <span className="field-line min-w-[150px]">{student.cell2}</span>
-                        </div>
-                        <div className="flex gap-4">
-                            <span className="font-bold text-[10px] ml-4">3.</span>
-                            <span className="field-line min-w-[150px]">{student.whatsapp}</span>
-                            <span className="font-bold text-[10px]">4.</span>
-                            <span className="field-line min-w-[150px]"></span>
+                    <div className="ml-4 border-l border-black pl-2">
+                        <span className="font-bold mr-1">Contact No:</span>
+                        <div className=" flex flex-col gap-2">
+                            <div className="flex gap-4">
+                                <span className="font-bold text-[10px]">1.</span>
+                                <span className="field-line w-[110px]">{student.cell1}</span>
+                                <span className="font-bold text-[10px]">2.</span>
+                                <span className="field-line w-[110px]">{student.cell2}</span>
+                            </div>
+                            <div className="flex gap-4">
+                                <span className="font-bold text-[10px]">3.</span>
+                                <span className="field-line w-[110px]">{student.whatsapp}</span>
+                                <span className="font-bold text-[10px]">4.</span>
+                                <span className="field-line w-[110px]"></span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
             {/* Declaration Section */}
-            <div className="border border-black p-1 mb-2 flex items-center">
+            <div className="border border-black p-2 mb-2 flex items-center">
                 <div className="section-header">Declaration</div>
                 <p className="text-[10px] italic flex-1 ml-2 font-bold">
                     It is hereby declared that above information is true and correct, I shall abide by the school regulations.
                 </p>
             </div>
-            <div className="flex justify-end mb-4 px-4">
-                <div className="flex flex-col items-center">
-                    <span className="field-line min-w-[200px] mb-1"></span>
+            <div className="flex justify-end mb-4 px-4 mt-5">
+                <div className="flex flex-row items-center gap-2">
                     <span className="font-bold">Sign. Parents / Guardian</span>
+                    <span className="flex flex-col items-center border-t border-black pt-1 mt-2 min-w-[150px]"></span>
+
                 </div>
             </div>
 
             {/* Provision Section */}
-            <div className="border border-black p-1 mb-2">
+            <div className="border border-black p-2 mb-2">
                 <div className="section-header mb-2">Provision</div>
                 <div className="flex justify-around py-1">
                     <div className="flex items-center">
@@ -294,30 +308,30 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
             </div>
 
             {/* Fees Detail Section */}
-            <div className="border border-black p-1 mb-10">
+            <div className="border border-black p-2 mb-10">
                 <div className="section-header mb-2">Fees Detail</div>
-                <div className="grid grid-cols-3 gap-y-3 px-4 py-2">
-                    <div className="flex items-end">
+                <div className="grid grid-cols-3 gap-y-3 px-4 py-2 gap-x-2">
+                    <div className="flex items-end gap-2">
                         <span className="font-bold mr-1">Admission Fee Rs:</span>
                         <span className="field-line flex-1">{student.admissionFee}</span>
                     </div>
-                    <div className="flex items-end">
+                    <div className="flex items-end gap-2">
                         <span className="font-bold mr-1">Annual Charges Rs:</span>
                         <span className="field-line flex-1">{student.annualCharges}</span>
                     </div>
-                    <div className="flex items-end">
+                    <div className="flex items-end gap-2">
                         <span className="font-bold mr-1">Monthly Fee Rs:</span>
                         <span className="field-line flex-1">{student.monthlyFee}</span>
                     </div>
-                    <div className="flex items-end">
+                    <div className="flex items-end gap-2">
                         <span className="font-bold mr-1">Academy Fee Rs:</span>
                         <span className="field-line flex-1">{student.academyFee}</span>
                     </div>
-                    <div className="flex items-end">
+                    <div className="flex items-end gap-2">
                         <span className="font-bold mr-1">Lab/ Misc Rs:</span>
                         <span className="field-line flex-1">{student.labMiscFee}</span>
                     </div>
-                    <div className="flex items-end">
+                    <div className="flex items-end gap-2">
                         <span className="font-bold mr-1">Total Amount Rs:</span>
                         <span className="field-line flex-1 font-bold">{totalFees}</span>
                     </div>
@@ -325,11 +339,16 @@ const PrintAdmissionForm = forwardRef(({ student, branchName }, ref) => {
             </div>
 
             {/* Footer */}
-            <div className="flex justify-end px-8 mt-12">
-                <div className="flex flex-col items-center border-t border-black pt-1 min-w-[150px]">
-                    <span className="font-bold">Signature Principal</span>
+            <div className="flex justify-end px-8 mt-12 gap-2">
+                <span className="font-bold">Signature Principal</span>
+                <div className="flex flex-col items-center border-t border-black pt-1 mt-3 min-w-[150px]">
+
                 </div>
             </div>
+            <div className="fixed bottom-0 left-0 right-0">
+                <PrintFooter />
+            </div>
+
         </div>
     );
 });
